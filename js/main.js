04d940d6292e4493049042069e2c3112a72e1f6f
torrentslover <110337417+torrentslover@users.noreply.github.com>
2022-07-31 15:27:49 +0200

    new file

---
+++ js/main.js
@@ -0,0 +1,6 @@
+window.onload = function()
+{     
+   var searchBox =  document.getElementById("gsc-i-id1");
+   searchBox.placeholder="Search Khojo or type a URL";
+   searchBox.title="Search...";
+}
